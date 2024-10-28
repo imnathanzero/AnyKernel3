aedec33c41d3e6e371d6d2b30dc8e036b64bdc8d
Nathan Andrew <nathannxx@proton.me>
2024-10-29 03:06:19 +0700

    nethunter

--- anykernel.sh
+++ anykernel.sh
@@ -1,14 +1,15 @@
 # AnyKernel3 Ramdisk Mod Script
 # osm0sis @ xda-developers
-
+# Changes for SkyDragon by HolyAngel @ xda-developers
+# Changes for NetHunter by Re4son
 
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=Hatsune Kernel
-do.devicecheck=0
-do.modules=0
-do.systemless=1
+kernel.string=NetHunter Kernel-defauwt-Kewnyew
+do.devicecheck=1
+do.modules=1
+do.systemless=0 #Never use this for NetHunter kernels as it prevents us from writing to /lib/modules
 do.cleanup=1
 do.cleanuponabort=0
 device.name1=RMX1941
@@ -22,22 +23,28 @@ block=/dev/block/by-name/boot;
 is_slot_device=0;
 ramdisk_compression=auto;
 
-
 ## AnyKernel methods (DO NOT CHANGE)
 # import patching functions/variables - see for reference
 . tools/ak3-core.sh;
 
+## Trim partitions
+fstrim -v /cache;
+fstrim -v /data;
 
 ## AnyKernel file attributes
 # set permissions/ownership for included ramdisk files
-set_perm_recursive 0 0 755 644 $ramdisk/*;
-set_perm_recursive 0 0 750 750 $ramdisk/init* $ramdisk/sbin;
-
+chmod -R 750 $ramdisk/*;
+chown -R root:root $ramdisk/*;
 
 ## AnyKernel install
 dump_boot;
 
 # begin ramdisk changes
+
+# init.rc
+# backup_file init.rc;
+# replace_string init.rc "cpuctl cpu,timer_slack" "mount cgroup none /dev/cpuctl cpu" "mount cgroup none /dev/cpuctl cpu,timer_slack";
+
 
 # end ramdisk changes
 
